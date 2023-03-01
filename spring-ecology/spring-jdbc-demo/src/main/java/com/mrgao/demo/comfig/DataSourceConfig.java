036f427ecd4aa9fd3e330d6a825b5126170fd172
Mr.Gao <862230476@qq.com>
2023-03-01 19:06:09 +0800

    添加数据库连接配置

--- spring-ecology/spring-jdbc-demo/src/main/java/com/mrgao/demo/comfig/DataSourceConfig.java
+++ spring-ecology/spring-jdbc-demo/src/main/java/com/mrgao/demo/comfig/DataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.mrgao.demo.comfig;
 
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,28 @@ import javax.sql.DataSource;
 public class DataSourceConfig {
 
 
+    /**
+     * 数据源1
+     *
+     * @return
+     */
     @Bean
-    @ConfigurationProperties(prefix = "")
-    public DataSource dataSource() {
-        //DruidDataSource
-        //DruidDataSourceFactory.createDataSource()
-        return null;
+    @ConfigurationProperties(prefix = "spring.datasource.datasource1")
+    public DataSource dataSource1() {
+        // 底层会拿到spring.datasource中的配置，创建一个DruidDataSource
+        return DruidDataSourceBuilder.create().build();
+    }
+
+
+    /**
+     * 数据源2
+     *
+     * @return
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.datasource2")
+    public DataSource dataSource2() {
+        // 底层会拿到spring.datasource中的配置，创建一个DruidDataSource
+        return DruidDataSourceBuilder.create().build();
     }
 }
